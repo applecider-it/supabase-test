bf927f8ac770f370d1c55156bc28b83e894ed1fc
owner <owner@localhost>
2026-04-19 18:05:27 +0900

    supabase動作確認

--- src/src/views/HomeView.vue
+++ src/src/views/HomeView.vue
@@ -1,4 +1,21 @@
 <script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabasePublishableKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
+
+console.log(supabaseUrl, supabasePublishableKey)
+
+const supabase = createClient(supabaseUrl, supabasePublishableKey)
+
+async function getInstruments() {
+  const { data } = await supabase.from('comments').select()
+  console.log(data);
+}
+onMounted(() => {
+   getInstruments()
+})
 </script>
 
 <template>
